ae13b82dcf740f69ead560f7d56fadddee1d57bd
WolffDev <info@davidwolff.dk>
2016-03-31 20:35:02 +0200

    Added Google Analytics

--- functions.php
+++ functions.php
@@ -118,6 +118,8 @@ function eal4th_scripts() {
 
 	wp_enqueue_script( 'eal4th-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
+		wp_enqueue_script( 'googleAnalytics', get_template_directory_uri() . '/js/googleAnalytics.js', array(), '20151215', true );
+
 	wp_enqueue_script( 'eal4th-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
