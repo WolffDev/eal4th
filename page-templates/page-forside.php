f643f49a7dad45913a14f026f6dfb3bd10342440
WolffDev <info@davidwolff.dk>
2016-05-14 21:37:26 +0200

    frontpage styling is completely done

--- page-templates/page-forside.php
+++ page-templates/page-forside.php
@@ -9,17 +9,15 @@ get_header(); ?>
 		<?php $image_id = get_post_thumbnail_id(); $image_url = wp_get_attachment_image_src($image_id,'post', true); ?>
 
 		<div class="top-image" style="background-image: url(<?php echo $image_url[0];?>);"></div>
-		
+
 		<main id="frontpage" class="frontpage-wrapper" role="main">
 
 			<div class="abn-ref-link">
-				<div class="text-vh-align">
-					<a href="#">
-						<p>
-							Se abonnementsløsninger
-						</p>
-					</a>
-				</div>
+				<a href="#">
+					<div class="text-vh-align">
+						<p>Se abonnementsløsninger</p>
+					</div>
+				</a>
 			</div>
 
 			<div class="user-select">
@@ -31,32 +29,27 @@ get_header(); ?>
 				<div class="user-select-wrapper">
 					<div class="user-column">
 						<?php
-				        $args = array( 'post_type' => 'product', 'posts_per_page' => 3, 'product_cat' => 'user-votes', 'orderby' => 'asc' );
+				        $args = array( 'post_type' => 'product', 'posts_per_page' => 6, 'product_cat' => 'user-votes', 'orderby' => 'asc' );
 				        $loop = new WP_Query( $args );
 				        while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
 								<div>
                   <a href="<?php echo get_permalink( $loop->post->ID ) ?>" title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>">
 
-                    <!-- <?php woocommerce_show_product_sale_flash( $post, $product ); ?> -->
+                    <?php // woocommerce_show_product_sale_flash( $post, $product ); ?>
 
                     <?php echo get_the_post_thumbnail($loop->post->ID, 'custom-size'); ?>
 
-                    <h3 class="hide"><?php the_title(); ?></h3>
+                    <!-- <h3 class="hide"><?php the_title(); ?></h3>
 
-                  	<span class="hide price"><?php echo $product->get_price_html(); ?></span>
+                  	<span class="hide price"><?php // echo $product->get_price_html(); ?></span> -->
 
                   </a>
 
-                  <!-- <?php woocommerce_template_loop_add_to_cart( $loop->post, $product ); ?> -->
+                  <?php // woocommerce_template_loop_add_to_cart( $loop->post, $product ); ?>
 								</div>
 
 				    <?php endwhile; ?>
 				    <?php wp_reset_query(); ?>
-					</div>
-					<div class="user-column">
-						<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo quos ratione id facere quod laudantium soluta quam deserunt eaque quia, vitae sapiente, voluptas nesciunt repudiandae.</div>
-						<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis natus veniam et magnam, facilis necessitatibus explicabo, libero minima exercitationem consectetur? Consectetur id aperiam soluta, hic quod aliquam ipsum?</div>
-						<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus aperiam voluptas quis voluptatum blanditiis explicabo commodi sapiente odit accusantium distinctio! Eos tenetur praesentium, itaque odit explicabo excepturi pariatur ipsum. In, omnis, tempore quod, illo ipsum voluptatem pariatur delectus hic, amet necessitatibus molestias!</div>
 					</div>
 				</div>
 			</div>
