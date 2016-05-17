b5419611f3618eb4c3fa94d5e0acf83384ca8248
WolffDev <info@davidwolff.dk>
2016-05-17 13:40:13 +0200

    styling...

--- page-templates/page-forside.php
+++ page-templates/page-forside.php
@@ -8,9 +8,31 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<?php $image_id = get_post_thumbnail_id(); $image_url = wp_get_attachment_image_src($image_id,'post', true); ?>
 
-		<div class="top-image" style="background-image: url(<?php echo $image_url[0];?>);"></div>
+		<div class="top-image" style="background-image: url(<?php echo $image_url[0];?>);">
+			<h1 class="top-phrase hide-on-med-and-down">Ét splitsekund er alt du behøver</h1>
+			<h2 class="sup-phrase hide-on-med-and-down">Direkte overførelse af kunstværker,<br>nemt, bekvæmt & hurtig</h2>
+			<div class="btn hide-on-med-and-down"></div>
+		</div>
 
 		<main id="frontpage" class="frontpage-wrapper" role="main">
+
+			<div class="steps-wrap hide-on-med-and-down">
+				<div class="step">
+					<img src="http://lorempixel.com/180/180/nature/1" alt=""></img>
+					<h2>Find dit mortiv</h2>
+					<p>Find det motiv du ønsker, de er alle af danske kunstnere som er i fuld gang med, at opbygge deres entré på det danske marked.</p>
+				</div>
+				<div class="step">
+					<img src="http://lorempixel.com/180/180/nature/2" alt=""></img>
+					<h2>Du modtager filen</h2>
+					<p>Du modtager billedfilen ved betaling, som du har muligheden for at benøtte som du vil.<br>Til det produkt du ønsker.</p>
+				</div>
+				<div class="step">
+					<img src="http://lorempixel.com/180/180/nature/3" alt=""></img>
+					<h2>Du bestemmer</h2>
+					<p>Dit billede kan bruges til mange forskellige ting - plakater, folie, krus mv.<br>Enten igennem vores samarbejdspartnerer eller selv.</p>
+				</div>
+			</div>
 
 			<div class="abn-ref-link">
 				<a href="#">
