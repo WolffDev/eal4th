ca39e05fb1ca46b404124191a3aef48a603b3d3b
WolffDev <info@davidwolff.dk>
2016-05-19 22:05:46 +0200

    added hotjar.js to script.js

--- js/script.js
+++ js/script.js
@@ -45,3 +45,13 @@ jQuery(document).ready(function($){
 
 
 });
+
+// Hotjar Tracking Code for http://www.artea.dk
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:213082,hjsv:5};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
